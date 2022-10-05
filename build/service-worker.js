4a3074b67737e3bede198fe3b0ca54b6a0853122
Uche Ngadi <uchengadi@yahoo.com>
2022-10-05 15:53:06 +0100

    api link updated

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01695a78f32ba0d5650be4f366338992.js"
+  "/precache-manifest.8e0653f7eca55ce6e13d413b765daa22.js"
 );
 
 self.addEventListener('message', (event) => {
