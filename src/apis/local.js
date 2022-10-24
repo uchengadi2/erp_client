7cacb1286181c511bde7c3d6d541e3ad9ba38f45
Uche Ngadi <uchengadi@yahoo.com>
2022-10-24 16:52:33 +0100

    repoint to production api

--- src/apis/local.js
+++ src/apis/local.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://localhost:8000/api/v1",
-  baseURL: "http://35.180.217.158/api/v1",
+  baseURL: "https://api.uudara.com/api/v1",
 });
